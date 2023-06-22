eea69c1b2b064c708d8086c253d0d2ae04a73a7b
michellekoegelenberg <137388896+alex-steele-here@users.noreply.github.com>
2023-06-22 13:10:18 +0200

    proof of work
    Adding proof of work implementing consensus in a golang blockchain.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/alex-steele-here/go-blockchain.git/blockchain"
 )
@@ -14,8 +15,14 @@ func main() {
 	chain.AddBlock("Third Block after Genesis")
 
 	for _, block := range chain.Blocks {
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 }
